4ca9fad192dfab9ec71a660e10eee5c548d4c3a2
y4xu <y4xu@eng.ucsd.edu>
2019-11-08 16:56:21 -0800

    multithread and mapping added

--- include/memory/mq.h
+++ include/memory/mq.h
@@ -2,6 +2,8 @@
 #define _MEMORY_MQ_H_
 
 #include <lego/list.h>
+#include <lego/mutex.h>
+#include <lego/slab.h>
 
 typedef struct list_data{
 	struct list_head *list;
@@ -12,9 +14,19 @@ typedef struct mq_header{
 	struct list_data *msg_list;
 	struct list_data *current_msg;
 	struct list_data *next_msg;
+	struct mutex mutex;
 	char* mq_name;
 	int msg_size;
 }mq_header;
+
+typedef struct addrmap{
+	char* mq_name;
+	void* mq_addr;
+}addrmap;
+
+struct addrmap *addr_map;
+int current_mq;
+int mq_size;
 
 static inline void listdata_add_tail(struct list_data *node, struct list_data *head){
 	list_add_tail(node->list, head->list);
@@ -22,4 +34,7 @@ static inline void listdata_add_tail(struct list_data *node, struct list_data *h
 
 static inline void listdata_del(struct list_data *node){
 	list_del(node->list);
+	kfree(node->data);
 }
+
+#endif
