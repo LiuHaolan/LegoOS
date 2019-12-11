78369495f2d1001b48e87df2ce79461d2fc9903d
haolan <3140101098@zju.edu.cn>
2019-12-11 01:17:41 -0500

    test scalability

--- usr/mq_send_test.c
+++ usr/mq_send_test.c
@@ -6,7 +6,41 @@
 #include <linux/unistd.h>
 #include "includeme.h"
 
+int main(void)
+{
+	printf("mq_open: %d\n", mq_open("cse", 100));
+	char* q = "yixu's joining";
+	printf("mq_send: %d\n", mq_send("cse",q, strlen(q)));
+	mq_open("ece", 80);
 
+	time_t t1 = time(NULL);
+	int j = 0;
+	for(j=0;j<2000;j++){
+		mq_send("ece", "Avada!", 6);
+	}
+	time_t t2 = time(NULL);
+	printf("time: %lld\n", t2-t1);
+
+/*
+	mq_send("ece", "damn!",5);
+	mq_send("ece", "LordVoldemortIsBack",19);
+	char* p1 = malloc(sizeof(char)*100);
+	int size1 = 1;
+	mq_receive("ece", p1, &size1);   
+	printf("string: %s", p1);
+	printf("str size: %lu\n" ,size1);	 
+	
+	char* p = malloc(sizeof(char)*100);
+	int size = 1;
+	mq_receive("cse", p, &size);  
+	printf("string: %s", p);
+	printf("str size: %lu\n" ,size);	 
+*/
+	mq_close("ece");
+
+}
+
+/*
 int main(void)
 {
 	printf("mq_open: %d\n", mq_open("cse", 100));
@@ -31,3 +65,5 @@ int main(void)
 	mq_close("cse");
 
 }
+
+*/
