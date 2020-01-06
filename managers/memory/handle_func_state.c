6a66947d224e561831b2036c4db1a6485de44633
LiuHaolan <3140101098@zju.edu.cn>
2020-01-06 03:21:22 -0500

    backup

--- managers/memory/handle_func_state.c
+++ managers/memory/handle_func_state.c
@@ -25,9 +25,120 @@
 #include <memory/replica.h>
 #include <memory/thread_pool.h>
 
+/*
+ * This one is adapted from do_munmap
+ * But when it works on the page table, it reserve certain pages and...
+ *
+ */ 
+int do_unmap_and_save(struct lego_mm_struct *mm, unsigned long start, size_t len){
+	unsigned long end;
+	struct vm_area_struct * vma, *prev, *last;
+
+	if((offset_in_page(start)) || start > TASK_SIZE || len > TASK_SIZE-start)
+	return -EINVAL;
+
+	len = PAGE_ALIGN(len);
+	if( len == 0)
+		return -EINVAL;
+
+	/* Find the first overlapping VMA */
+	vma = find_vma(mm, start);
+	if(!vma)
+		return 0;
+	prev = vma->vm_prev;
+	/* for this start < vm->vm_end */
+
+	/* not overlapped */
+	end = start + len;
+	if(vma->vm_start >= end)
+		return 0;	
+
+	/* split any vma */
+	if(start > vma->vm_start){
+		int error;
+
+		/*
+ * 		 * Make sure that map_count on return from munmap() will
+ * 		 		 * not exceed its limit; but let map_count go just above
+ * 		 		 		 * its limit temporarily, to help free resources as expected.
+ * 		 		 		 		 */
+		if (end < vma->vm_end && mm->map_count >= sysctl_max_map_count)
+			return -ENOMEM;
+
+		error = __split_vma(mm, vma, start, 0);
+		if (error)
+			return error;
+		prev = vma;
+#ifdef CONFIG_DISTRIBUTED_VMA_MEMORY
+//		save_vma_context(mm, mm->vmrange_map[vmr_idx(start)]);
+	// not sure what to do here right now
+#endif	
+	}
+
+	/* Does it split the last one? */
+	last = find_vma(mm, end);
+	if (last && end > last->vm_start) {
+		int error = __split_vma(mm, last, end, 1);
+		if (error)
+			return error;
+	}
+	vma = prev ? prev->vm_next : mm->mmap;
+
+	/* remove the vma */
+	detach_vmas_to_be_unmapped(mm, vma, prev,end);
+	save_region(mm, vma, prev,start, end);;
+
+	/* Fix up all other VM information */
+	remove_vma_list(mm,vma);
+
+	return 0;
+}
+
 void handle_p2m_vms_save(struct p2m_mmap_struct *payload, struct common_header *hdr, struct thpool_buffer *tb)
 {
+	u32 nid = hdr->src_nid;
+	u32 pid = payload->pid;
+
+	unsigned long addr = payload->addr;
+	unsigned long len = payload->len;
+	struct lego_task_struct *tsk;
+	struct lego_mm_struct *mm;	
+
+	tsk = find_lego_task_by_pid(nid, pid);
+	if (unlikely(!tsk)) {
+//		reply->ret = -RET_ESRCH;
+		return;
+	}
 	
+	mm = tsk->mm;
+	if(down_write_killable(&mm->mmap_sem)) {
+//		reply->ret = RET_EINTR;
+		return;
+	}
+
+	/*
+ 	* config the distributed vma memory or not
+ 	*/
+
+#ifdef CONFIG_DISTRIBUTED_VMA_MEMORY
+
+#else
+	/* 
+ 	 *
+ 	 * save the page table*/
+	do_unmap_and_save(mm, addr, len);
+
+#endif
+
+	up_write(&mm->mmap_sem);
+
+	/* 
+ 	* you need to change the return value since other is done
+ 	*/
+  
+	size_t* retval;
+	*retval = 1;
+	tb_set_tx_size(tb, sizeof(*retval));	
 
 }
 
